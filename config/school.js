48a0dbc543fe7ab72ca46c0091f8fe875f195e9f
damilaredaniel05-eng <damilaredaniel05@gmail.com>
2026-06-04 13:37:09 +0100

    Add school configuration file

---
+++ config/school.js
@@ -0,0 +1,8 @@
+module.exports = {
+  schoolName: 'Your School Name Here',
+  schoolLogo: '/images/logo.png',
+  backgroundImage: '/images/background.jpg',
+  schoolEmail: 'info@school.com',
+  schoolPhone: '+234-XXX-XXX-XXXX',
+  schoolAddress: 'School Address Here'
+};
